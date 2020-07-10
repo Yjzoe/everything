93588faae9925d42377fbccd6aa7530702f1f71b
yj <anuefn@naverc.om>
2020-07-10 18:07:38 +0900

    서버로 이미지 업로드

--- src/main/java/com/review/everything/config/WebConfig.java
+++ src/main/java/com/review/everything/config/WebConfig.java
@@ -2,9 +2,12 @@ package com.review.everything.config;
 
 import com.review.everything.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.util.List;
 
@@ -12,6 +15,17 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Value("${storage.location}")
+    private String fileRealPath;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+
+        registry.addResourceHandler("/uploads/**").addResourceLocations("file:///" + fileRealPath).setCachePeriod(3600)
+                .resourceChain(true).addResolver(new PathResourceResolver());
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
---
+++ src/main/java/com/review/everything/web/ImageController.java
@@ -0,0 +1,43 @@
+package com.review.everything.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Controller
+public class ImageController {
+
+    @Value("${storage.location}")
+    private String fileRealPath;
+
+    @PutMapping("/image/upload")
+    public @ResponseBody
+    String imageUpload(@RequestParam("imgFile") MultipartFile imgFile) {
+        try {
+            System.out.println(imgFile.getBytes().toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //파일명
+        UUID uuid = UUID.randomUUID();
+        String uuidFilename = uuid + "_" + imgFile.getOriginalFilename();
+
+        Path filePath = Paths.get(fileRealPath + uuidFilename);
+
+        try {
+            Files.write(filePath, imgFile.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "ok";
+    }
+}
